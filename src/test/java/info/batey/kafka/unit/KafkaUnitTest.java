f07344ecfe6ed119430e30cd41c4da62ffe59fa8
Michal Michalski <github@michalski.im>
2015-12-17 15:17:23 +0000

    Implement alternative constructor to allow passing a connection string (host:port)

---
+++ src/test/java/info/batey/kafka/unit/KafkaUnitTest.java
@@ -0,0 +1,42 @@
+package info.batey.kafka.unit;
+
+
+import org.junit.Test;
+
+public class KafkaUnitTest {
+
+    @Test
+    public void successfullyConstructKafkaUnitFromConnectionStrings() {
+        new KafkaUnit("localhost:2181", "localhost:9092");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromZooKeeperConnectionStringWithNonNumericPort() {
+        new KafkaUnit("localhost:abcd", "localhost:9092");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromKafkaConnectionStringWithNonNumericPort() {
+        new KafkaUnit("localhost:2181", "localhost:abcd");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromZooKeeperConnectionStringWithMultipleHosts() {
+        new KafkaUnit("localhost:2181,localhost:2182", "localhost:9092");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromKafkaConnectionStringWithMultipleHosts() {
+        new KafkaUnit("localhost:2181", "localhost:9092,localhost:9093");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromInvalidZooKeeperConnectionString() {
+        new KafkaUnit("localhost-2181", "localhost:9092");
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void failToConstructKafkaUnitFromInvalidKafkaConnectionString() {
+        new KafkaUnit("localhost:2181", "localhost-9092");
+    }
+}
